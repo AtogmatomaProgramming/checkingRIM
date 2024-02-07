e637160e672094eafa93d2aa410fa49ee8043da9
Marco A. Ámez <marco.a.amez@gmail.com>
2024-02-07 15:28:07 +0100

    Improve documentation.

--- README.md
+++ README.md
@@ -1,36 +1,73 @@
-# Script para generar los ficheros excel usados en el chequeo de los muestreos MT2 de RIM.
+# Generación de ficheros excel para el chequeo de los muestreos MT2 de RIM.
+Este script genera los ficheros excel que se usan para realizar el chequeo de 
+los muestreos MT2 de RIM.
 
 Se crean los ficheros para chequear:
-- cabeceras de los muestreos (check_headers_2022_01.xlsx)
-- pesos desembarcados por categoría (check_catches_2022_01.xlsx)
-- tallas (check_lengths_2022_01.xlsx)
-
+- las cabeceras de los muestreos (check_headers_2022_01.xlsx)
+- los pesos desembarcados por categoría (check_catches_2022_01.xlsx)
+- las tallas (check_lengths_2022_01.xlsx)
 
 Cada fichero contiene los campos necesarios que hay que comprobar manualmente 
 para considerar los muestreos chequeados.
 
 
-El script necesita dos de los informes 'tallas por up' que se obtienen del SIRENO.
+## Requisitos
+Son necesarias las librerías de R *sapmuebase*, *dplyr*, *openxlsx* y *pivottabler*.
+
+Este script consta de los archivos chequeo_muestreos_MT2.R y functions.R.
+
+El script requiere dos de los informes '*tallas por up*' que se obtienen del SIRENO.
 En concreto:
 - capturas (IEOUPMUEDESTOT.TXT)
 - capturas de especies medidas (IEOUPDESTAL.TXT)
 
+Estos informes deben estar ubicados en la carpeta YEAR_MONTH (ver apartado siguiente).
 
-La estructura de las carpetas para que el script funcione adecuadamente es:
- ↳ (carpeta donde se encuentra el script)
-     ↳ data
-        ↳ YEAR
-            ↳ YEAR_MONTH
+
+### Estructura de las carpetas
+Para el correcto funcionamiento del script se debe tener la siguiente estructura
+de carpetas:
+```
+↳ (carpeta donde se encuentra el script)
+      ↳ data
+          ↳ YEAR
+              ↳ YEAR_MONTH
+```
 Siendo YEAR el año y MONTH el mes en dígitos. Por ejemplo:
 
 
 ![Ejemplo de estructura de la carpetas.](./assets/folders_example.png)
 
+Así, la ruta del archivo de cabeceras del mes de enero de 2022 sería:
+```data/2022/2022_01/check_headers_2022_01.xlsx```
 
 
 Los informes 'tallas por up' deben estar en la carpeta YEAR_MONTH.
 
 
-Los ficheros generados se guardan en la carpeta YEAR_MONTH.
+## Ejecución del script
+Se deben ejecutar todas las líneas de código del fichero chequeo_muestreos_MT2.R.
+
+El script genera los ficheros de chequeo para todos los datos que haya en los
+informes de '*tallas por up*'. No realiza ningún filtrado por fecha. Por lo tanto,
+si los informes del SIRENO tienen datos de varios meses, los ficheros generados
+tendrán información de esos meses.
+
+Sólo es necesario cambiar las variables del apartado `## YOU ONLY HAVE TO CHANGE
+THIS VARIABLES`. El resto del script no hay que modificarlo.
+
+
+## Ficheros finales
+Los ficheros generados se guardan en la carpeta YEAR_MONTH con formato .xlsx.
+En el caso de que alguno de los ficheros exista, el script preguntará al usuario
+si desea reemplazarlos.
+
+Los ficheros generados son:
+- check_headers_YEAR_MONTH.xlsx
+- check_catches_YEAR_MONTH.xlsx
+- check_lengths_YEAR_MONTH.xlsx
+
+Por ejemplo: check_headers_2022_01.xlsx, check_catches_2022_01.xlsx y
+check_lengths_2022_01.xlsx
 
 
--- chequeo_muestreos_MT2.R
+++ chequeo_muestreos_MT2.R
@@ -1,28 +1,39 @@
-# CHECK MT2 SAMPLES ----
-
+# CHECK MT2 SAMPLES ------------------------------------------------------------
 ## INSTRUCTIONS ----------------------------------------------------------------
-# This script create 3 xlsx files used to manual checking of MT2 samples:
-# - file to check headers of the samples: check_headers_YEAR_MONTH.xlsx
-# - file to check catches of the samples: check_catches_YEAR_MONTH.xlsx
-# - file to check lengths of the samples: check_lengths_YEAR_MONTH.xlsx
+# Script para generar los ficheros excel usados en el chequeo de los muestreos MT2 de RIM.
 #
-# Every xlsx file contain the mandatory variables which must be manually checked.
+# Puedes encontrar instrucciones más detalladas en el fichero README.md.
+# 
+# Se crean los ficheros para chequear:
+#   - cabeceras de los muestreos (check_headers_2022_01.xlsx)
+#   - pesos desembarcados por categoría (check_catches_2022_01.xlsx)
+#   - tallas (check_lengths_2022_01.xlsx)
+# 
+# Cada fichero contiene los campos necesarios que hay que comprobar manualmente 
+# para considerar los muestreos chequeados.
+# 
+# El script necesita dos de los informes 'tallas por up' que se obtienen del SIRENO.
+# En concreto:
+#   - capturas (IEOUPMUEDESTOT.TXT)
+#   - capturas de especies medidas (IEOUPDESTAL.TXT)
+# 
+# La estructura de las carpetas para que el script funcione adecuadamente es:
+#   ↳ (carpeta donde se encuentra el script)
+#       ↳ data
+#           ↳ YEAR
+#               ↳ YEAR_MONTH
 #
-# The scripts needs the SIRENO reports:
-# - catches 
-# - catches in lengths
+# Siendo YEAR el año y MONTH el mes en dígitos.
 #
-# The folder structure use in this script is:
-# ↳ folder containing this script
-#     ↳ data
-#         ↳ YEAR
-#            ↳ YEAR_MONTH
+# Por ejemplo, la ruta del archivo de cabeceras del mes de enero de 2022 sería:
+# data/2022/2022_01/check_headers_2022_01.xlsx. 
 #
-# The SIRENO files must be stored in the YEAR_MONTH folder.
+# Puedes ver una imagen de cómo sería esta estructura de carpetas en el archivo
+# /assets/folders_example.png.
 #
-# The xlsx files created by this script will be saved in the YEAR_MONTH folder.
-
-
+# Los informes 'tallas por up' deben estar en la carpeta YEAR_MONTH.
+# 
+# Los ficheros generados se guardan en la carpeta YEAR_MONTH.
 
 
 ## LIBRARIES --------------------------------------------------------------------
@@ -32,23 +43,31 @@ library(openxlsx)
 library(pivottabler)
 source("functions.R")
 
-## YOU ONLY HAVE TO CHANGE THIS VARIABLES ---------------------------------------
 
+## YOU ONLY HAVE TO CHANGE THIS VARIABLES ---------------------------------------
+# Year to check
 YEAR <- "2022"
+
+# Month to check, in digits.
 MONTH <- "01"
 
+# File names of SIRENO reports
 FILENAME_DES_TOT <- paste0("IEOUPMUEDESTOTMARCO.TXT")
 FILENAME_DES_TAL <- paste0("IEOUPMUEDESTALMARCO.TXT")
 
+# Ports you want to check. Pay attention to spelling.
 MY_PORTS <- c("Santoña", "San Vicente de la Barquera", "Llanes", "Suances", "Santander")
+
 
 ## GLOBAL VARIABLES - DON'T CHANGE IT -------------------------------------------
 PATH_FILES <- paste0(getwd(), YEAR, "/", MONTH)
 DATA_PATH <- paste0(getwd(),"/data/", YEAR, "/", YEAR, "_", MONTH)
 
+
 ## IMPORT DATA
 capturas_tot <- importRIMCatches(FILENAME_DES_TOT, path = DATA_PATH)
 tallas <- importRIMCatchesInLengths(FILENAME_DES_TAL, path = DATA_PATH)
+
 
 # CREATE FILES
 createCheckFilesXlsx(capturas_tot, tallas)
